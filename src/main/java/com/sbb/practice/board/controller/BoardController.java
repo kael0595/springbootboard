ac2350807e40e9b875f1c2a509adc50016c0c1a5
kael0595 <changgyu549@gmail.com>
2024-08-27 15:57:38 +0900

    BoardController writing

---
+++ src/main/java/com/sbb/practice/board/controller/BoardController.java
@@ -0,0 +1,11 @@
+package com.sbb.practice.board.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/board")
+public class BoardController {
+}
